0183376793f8f1441014eb67e15ecc6d0eea782b
Damian Socha <damian.socha@amigo.ie>
2024-03-28 16:01:16 +0000

    Revert "second commit"
    This reverts commit 2483cfd447523c58813fa77ec17a1c0dcc19df07.

--- Loops/generators in for in expressions.py
+++
@@ -1,3 +0,0 @@
-nums = (2, 5, 6, 8, 9)
-squares = (pow(num, 2) for num in nums)
-print(squares)
--- Ternary Operator/tern_op.py
+++
@@ -1,6 +0,0 @@
-# Expression_1 if Condition else Expression_2
-
-number = 23
-
-result = f"Number {number} is even!" if number % 2 == 0 else f"Number {number} is odd!"
-print(result)
